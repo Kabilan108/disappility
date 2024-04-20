6fda3a36169e0c619b1ae27d689ca9e5dd6d2d04
Tony Okeke <tonykabilanokeke@gmail.com>
2024-04-20 15:02:47 -0400

    add piper tts

---
+++ tts/download-model.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# kathleen (low) - 16000 Hz
+# MODEL_URL="https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/kathleen/low/en_US-kathleen-low.onnx"
+# CONFIG_URL="https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/kathleen/low/en_US-kathleen-low.onnx.json"
+
+# hfc-female (medium) - 22050 Hz
+MODEL_URL="https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/hfc_female/medium/en_US-hfc_female-medium.onnx"
+CONFIG_URL="https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/hfc_female/medium/en_US-hfc_female-medium.onnx.json"
+
+wget -P "$(dirname "$0")" $MODEL_URL
+wget -P "$(dirname "$0")" $CONFIG_URL
