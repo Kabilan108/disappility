7fbfa3facd69e33978c99650814e18366fb9acac
Dalu Okonkwo <chukdals@gmail.com>
2024-04-21 07:47:45 -0400

    added electron frontend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
+import path from "path"
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [react()],
+    server: {
+        port: 3000,
+    },
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    plugins: [react()],
 });
+
+
